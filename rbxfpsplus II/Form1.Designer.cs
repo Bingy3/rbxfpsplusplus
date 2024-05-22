5e7324ba7885b605333f1b3698d44b141d3661b4
bobwe <bobwe@JIMMYNEWTRON>
2024-05-21 21:26:19 -0700

    Add project files.

---
+++ rbxfpsplus II/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace rbxfpsplus_II
+{
+    partial class RBXFPSPlus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RBXFPSPlus));
+            this.Title = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.DragPanel = new System.Windows.Forms.Panel();
+            this.MinimizeButton = new System.Windows.Forms.Button();
+            this.CurrentPath = new System.Windows.Forms.Label();
+            this.GetRobloxPath = new System.Windows.Forms.Button();
+            this.GenFpsUnlocker = new System.Windows.Forms.Button();
+            this.CustomPath = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(2, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(175, 49);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "RBXFPS+";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitButton.FlatAppearance.BorderSize = 0;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.Font = new System.Drawing.Font("Segoe UI Semibold", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.ExitButton.Location = new System.Drawing.Point(741, 0);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(56, 58);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.Text = "X";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // DragPanel
+            // 
+            this.DragPanel.Location = new System.Drawing.Point(6, 0);
+            this.DragPanel.Name = "DragPanel";
+            this.DragPanel.Size = new System.Drawing.Size(791, 67);
+            this.DragPanel.TabIndex = 2;
+            this.DragPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseDown);
+            this.DragPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseMove);
+            this.DragPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.DragPanel_MouseUp);
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MinimizeButton.FlatAppearance.BorderSize = 0;
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinimizeButton.Font = new System.Drawing.Font("Segoe UI Semibold", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimizeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.MinimizeButton.Location = new System.Drawing.Point(689, 0);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(56, 58);
+            this.MinimizeButton.TabIndex = 3;
+            this.MinimizeButton.Text = "-";
+            this.MinimizeButton.UseVisualStyleBackColor = true;
+            this.MinimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // CurrentPath
+            // 
+            this.CurrentPath.AutoSize = true;
+            this.CurrentPath.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentPath.ForeColor = System.Drawing.Color.White;
+            this.CurrentPath.Location = new System.Drawing.Point(12, 307);
+            this.CurrentPath.Name = "CurrentPath";
+            this.CurrentPath.Size = new System.Drawing.Size(82, 28);
+            this.CurrentPath.TabIndex = 4;
+            this.CurrentPath.Text = "Status...";
+            // 
+            // GetRobloxPath
+            // 
+            this.GetRobloxPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.GetRobloxPath.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.GetRobloxPath.FlatAppearance.BorderSize = 0;
+            this.GetRobloxPath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GetRobloxPath.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GetRobloxPath.ForeColor = System.Drawing.Color.White;
+            this.GetRobloxPath.Location = new System.Drawing.Point(274, 113);
+            this.GetRobloxPath.Name = "GetRobloxPath";
+            this.GetRobloxPath.Size = new System.Drawing.Size(271, 49);
+            this.GetRobloxPath.TabIndex = 5;
+            this.GetRobloxPath.Text = "Get roblox path";
+            this.GetRobloxPath.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.GetRobloxPath.UseVisualStyleBackColor = false;
+            this.GetRobloxPath.Click += new System.EventHandler(this.GetRobloxPath_Click);
+            // 
+            // GenFpsUnlocker
+            // 
+            this.GenFpsUnlocker.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.GenFpsUnlocker.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.GenFpsUnlocker.FlatAppearance.BorderSize = 0;
+            this.GenFpsUnlocker.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GenFpsUnlocker.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenFpsUnlocker.ForeColor = System.Drawing.Color.White;
+            this.GenFpsUnlocker.Location = new System.Drawing.Point(226, 178);
+            this.GenFpsUnlocker.Name = "GenFpsUnlocker";
+            this.GenFpsUnlocker.Size = new System.Drawing.Size(371, 49);
+            this.GenFpsUnlocker.TabIndex = 6;
+            this.GenFpsUnlocker.Text = "Generate fps unlocker";
+            this.GenFpsUnlocker.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.GenFpsUnlocker.UseVisualStyleBackColor = false;
+            this.GenFpsUnlocker.Click += new System.EventHandler(this.GenFpsUnlocker_Click);
+            // 
+            // CustomPath
+            // 
+            this.CustomPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.CustomPath.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.CustomPath.FlatAppearance.BorderSize = 0;
+            this.CustomPath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CustomPath.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomPath.ForeColor = System.Drawing.Color.White;
+            this.CustomPath.Location = new System.Drawing.Point(274, 249);
+            this.CustomPath.Name = "CustomPath";
+            this.CustomPath.Size = new System.Drawing.Size(271, 49);
+            this.CustomPath.TabIndex = 7;
+            this.CustomPath.Text = "Custom path";
+            this.CustomPath.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.CustomPath.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.label1.Location = new System.Drawing.Point(7, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Made by Bingy3 (@AG_FROP on roblox)";
+            // 
+            // RBXFPSPlus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(800, 350);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CustomPath);
+            this.Controls.Add(this.GenFpsUnlocker);
+            this.Controls.Add(this.GetRobloxPath);
+            this.Controls.Add(this.CurrentPath);
+            this.Controls.Add(this.MinimizeButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.DragPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RBXFPSPlus";
+            this.Text = "RBXFPSPlus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Panel DragPanel;
+        private System.Windows.Forms.Button MinimizeButton;
+        private System.Windows.Forms.Label CurrentPath;
+        private System.Windows.Forms.Button GetRobloxPath;
+        private System.Windows.Forms.Button GenFpsUnlocker;
+        private System.Windows.Forms.Button CustomPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ rbxfpsplus II/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rbxfpsplus_II
+{
+    public partial class RBXFPSPlus : Form
+    {
+        public RBXFPSPlus()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        // dragging
+
+        private Point lastLoc;
+        private bool mouseDown;
+
+        private void DragPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLoc = e.Location;
+        }
+
+        private void DragPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown) { 
+                this.Location = new Point((this.Location.X - lastLoc.X) + e.X, (this.Location.Y - lastLoc.Y) + e.Y);
+                this.Update();
+            }
+        }
+
+        private void DragPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        // fps unlocker stuff
+
+        private String robloxPath;
+        private String customPath;
+
+        private String getRobloxPath(String _path) {
+            String path = "";
+            if (Directory.Exists(_path + "/Versions")) {
+                foreach (String __path in Directory.GetDirectories(_path + "/Versions")) {
+                    if (File.Exists(__path + "/RobloxPlayerBeta.exe")) {
+                        path = __path;
+                    }
+                }
+            }
+            return path;
+        }
+
+        /* json
+         
+         {
+            "DFIntTaskSchedulerTargetFps": 10000,
+            "FFlagReportFpsAndGfxQualityPercentiles": false
+         }
+         
+         */
+
+        private String[] json = {
+            "{",
+            "\"DFIntTaskSchedulerTargetFps\": 10000,",
+            "\"FFlagReportFpsAndGfxQualityPercentiles\": false",
+            "}"
+        };
+
+        private void createClientSettings(String rbxPath) {
+            if (!Directory.Exists(rbxPath + "/ClientSettings")) {
+                DirectoryInfo info = Directory.CreateDirectory(rbxPath + "/ClientSettings");
+                File.Create(info.FullName + "/ClientAppSettings.json").Close();
+                // writing to the file
+                File.WriteAllText(info.FullName + "/ClientAppSettings.json", "{\r\n            \"DFIntTaskSchedulerTargetFps\": 10000,\r\n            \"FFlagReportFpsAndGfxQualityPercentiles\": false\r\n         }");
+            } else {
+                this.CurrentPath.Text = "Fps unlocker already exist!";
+            }
+        }
+
+        private void GetRobloxPath_Click(object sender, EventArgs e)
+        {
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            path = Directory.GetParent(path).FullName;
+            if (Directory.Exists(path + "/Local"))
+            {
+                path = path + "/Local";
+                if (Directory.Exists(path + "/Roblox"))
+                {
+                    path = path + "/Roblox";
+                    robloxPath = getRobloxPath(path);
+                    if (robloxPath == "") { this.CurrentPath.Text = "Cannot find version folder!"; return; }
+                    this.CurrentPath.Text = robloxPath;
+                }
+                else
+                {
+                    this.CurrentPath.Text = "Roblox doesn't exist!";
+                }
+            } else {
+                this.CurrentPath.Text = "Cannot find /Local folder!";
+            }   
+        }
+
+        private void GenFpsUnlocker_Click(object sender, EventArgs e)
+        {
+            if (robloxPath != "" || robloxPath != null) { 
+                createClientSettings(robloxPath);
+            }
+        }
+    }
+}
